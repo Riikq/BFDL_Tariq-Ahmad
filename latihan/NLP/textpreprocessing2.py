7edf158c43d748e61f0647de9f9cf3fc7bf41826
Riikq <tariqahmad0510@gmail.com>
2026-04-22 21:52:19 +0700

    tokenization

--- latihan/NLP/textpreprocessing2.py
+++ latihan/NLP/textpreprocessing2.py
@@ -1,41 +1,27 @@
-import nltk
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import TreebankWordTokenizer
+import re
 
-# Download korpus stopwords bahasa Indonesia dari NLTK jika belum terunduh
-nltk.download("stopwords")
-nltk.download("punkt_tab")  # Untuk tokenisasi kata
+text = "Ini adalah contoh tokenisasi kata dalam pemrosesan teks. Apakah ini kalimat kedua? Ya, ini kalimat kedua."
+tokens = word_tokenize(text)
+print(tokens, "\n")
 
-teks = "Perekonomian Indonesia sedang dalam pertumbuhan yang membanggakan."
+sentences = sent_tokenize(text)
+print(sentences, "\n")
 
-# Tokenisasi teks menjadi kata-kata
-tokens_kata = word_tokenize(teks)
+# Misalkan kita ingin memisahkan frasa berdasarkan tanda baca koma
+text = "Pemrosesan teks adalah cabang ilmu komputer yang berfokus pada pengolahan teks dan dokumen."
+tokenizer = TreebankWordTokenizer()
+phrases = tokenizer.tokenize(text)
+print(phrases, "\n")
 
-# Ambil daftar stopwords bahasa Indonesia dari NLTK
-stopwords_indonesia = set(stopwords.words("indonesian"))
+text = "Pertama, kita perlu menyiapkan bahan-bahan yang diperlukan."
+tokens = re.findall(r"\w+|\d+", text)
+print(tokens, "\n")
 
-# Filtering kata-kata dengan menghapus stopwords
-kata_penting = [kata for kata in tokens_kata if kata.lower() not in stopwords_indonesia]
-
-# Gabungkan kata-kata penting kembali menjadi teks
-teks_tanpa_stopwords = " ".join(kata_penting)
-
-print("Teks asli: ", teks)
-print("Teks setelah filtering stopwords NLTK: ", teks_tanpa_stopwords)
-
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-
-# Inisialisasi objek StopWordRemover dari Sastrawi
-factory = StopWordRemoverFactory()
-stopwords_sastrawi = factory.get_stop_words()
-
-tokens_kata = word_tokenize(teks)
-
-# Filtering kata-kata dengan menghapus stopwords Sastrawi
-kata_penting = [kata for kata in tokens_kata if kata.lower() not in stopwords_sastrawi]
-
-# Gabungkan kata-kata penting kembali menjadi teks
-teks_tanpa_stopwords = " ".join(kata_penting)
-
-print("Teks asli: ", teks)
-print("Teks setelah filtering stopwords Sastrawi: ", teks_tanpa_stopwords)
+# Model-based Tokenization
+# Misalnya menggunakan spasi sebagai pemisah kata
+text = "Ini adalah contoh tokenisasi berbasis model."
+tokens = text.split()
+print(tokens, "\n")
