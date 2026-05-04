5ef6380910a052fb35651896a92b3327a435905a
Riikq <tariqahmad0510@gmail.com>
2026-05-04 09:35:50 +0700

    finished training

--- latihan/NLP/submission1/2pewdie.py
+++ latihan/NLP/submission1/2pewdie.py
@@ -10,6 +10,13 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import accuracy_score, precision_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
 nltk.download("stopwords")
 
@@ -115,8 +122,78 @@ def show_wordcloud(data, title):
     plt.show()
 
 
-show_wordcloud(clean_df["text_akhir"], "All Comments WordCloud")
-pos_data = clean_df[clean_df["polarity"] == "positive"]["text_akhir"]
-show_wordcloud(pos_data, "Positive Sentiment WordCloud")
-neg_data = clean_df[clean_df["polarity"] == "negative"]["text_akhir"]
-show_wordcloud(neg_data, "Negative Sentiment WordCloud")
+# show_wordcloud(clean_df["text_akhir"], "All Comments WordCloud")
+# pos_data = clean_df[clean_df["polarity"] == "positive"]["text_akhir"]
+# show_wordcloud(pos_data, "Positive Sentiment WordCloud")
+# neg_data = clean_df[clean_df["polarity"] == "negative"]["text_akhir"]
+# show_wordcloud(neg_data, "Negative Sentiment WordCloud")
+
+X = clean_df["text_akhir"]
+y = clean_df["polarity"]
+
+tfidf = TfidfVectorizer(max_features=200, min_df=17, max_df=0.8)
+X_tfidf = tfidf.fit_transform(X)
+
+features_df = pd.DataFrame(X_tfidf.toarray(), columns=tfidf.get_feature_names_out())
+
+features_df
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_tfidf, y, test_size=0.2, random_state=42
+)
+
+naive_bayes = BernoulliNB()
+
+naive_bayes.fit(X_train.toarray(), y_train)
+
+y_pred_train_nb = naive_bayes.predict(X_train.toarray())
+y_pred_test_nb = naive_bayes.predict(X_test.toarray())
+
+accuracy_train_nb = accuracy_score(y_pred_train_nb, y_train)
+accuracy_test_nb = accuracy_score(y_pred_test_nb, y_test)
+
+print("Naive Bayes - accuracy train: ", accuracy_train_nb)
+print("Naive Bayes - accuracy test: ", accuracy_test_nb)
+
+
+random_forest = RandomForestClassifier()
+
+random_forest.fit(X_train.toarray(), y_train)
+
+y_pred_train_rf = random_forest.predict(X_train.toarray())
+y_pred_test_rf = random_forest.predict(X_test.toarray())
+
+accuracy_train_rf = accuracy_score(y_pred_train_rf, y_train)
+accuracy_test_rf = accuracy_score(y_pred_test_rf, y_test)
+
+print("Random Forest - accuracy_train:", accuracy_train_rf)
+print("Random Forest - accuracy_test:", accuracy_test_rf)
+
+
+logistic_regression = LogisticRegression()
+
+logistic_regression.fit(X_train.toarray(), y_train)
+
+y_pred_train_lr = logistic_regression.predict(X_train.toarray())
+y_pred_test_lr = logistic_regression.predict(X_test.toarray())
+
+accuracy_train_lr = accuracy_score(y_pred_train_lr, y_train)
+
+accuracy_test_lr = accuracy_score(y_pred_test_lr, y_test)
+
+print("Logistic Regression - accuracy_train:", accuracy_train_lr)
+print("Logistic Regression - accuracy_test:", accuracy_test_lr)
+
+
+decision_tree = DecisionTreeClassifier()
+
+decision_tree.fit(X_train.toarray(), y_train)
+
+y_pred_train_dt = decision_tree.predict(X_train.toarray())
+y_pred_test_dt = decision_tree.predict(X_test.toarray())
+
+accuracy_train_dt = accuracy_score(y_pred_train_dt, y_train)
+accuracy_test_dt = accuracy_score(y_pred_test_dt, y_test)
+
+print("Decision Tree - accuracy_train:", accuracy_train_dt)
+print("Decision Tree - accuracy_test:", accuracy_test_dt)
